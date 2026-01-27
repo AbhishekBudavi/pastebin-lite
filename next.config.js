2a4d45ca3d5b5b8e44bd3af094b12ed8a4ed1238
AbhishekBudavi <budaviabhishek994@gmail.com>
2026-01-27 18:55:46 +0530

    Fix Vercel deployment: remove experimental appDir, use @vercel/postgres sql API

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
   },
